ece376d8967dd561d0ba1c51b277239c8c7f015a
Zdeno Kuzmany <zku@webmecanik.com>
2022-10-16 10:02:39 +0200

    Add features tab to plugin If need it

---
+++ Form/Type/ConfigFeaturesType.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MauticPlugin\SkeletonBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ConfigFeaturesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add(
+            'setting',
+            ChoiceType::class,
+            [
+                'label'             => 'skeleton.setting',
+                'label_attr'        => ['class' => 'control-label'],
+                'required'          => true,
+                'attr'              => [
+                    'class' => 'form-control',
+                ],
+                'choices'           => [
+                    'skeleton.hello'    => 'hello',
+                    'skeleton.hi'    => 'hi',
+                ],
+            ]
+        );
+    }
+}
--- Integration/Support/ConfigSupport.php
+++ Integration/Support/ConfigSupport.php
@@ -5,10 +5,17 @@ declare(strict_types=1);
 namespace MauticPlugin\SkeletonBundle\Integration\Support;
 
 use Mautic\IntegrationsBundle\Integration\DefaultConfigFormTrait;
+use Mautic\IntegrationsBundle\Integration\Interfaces\ConfigFormFeatureSettingsInterface;
 use Mautic\IntegrationsBundle\Integration\Interfaces\ConfigFormInterface;
+use MauticPlugin\SkeletonBundle\Form\Type\ConfigFeaturesType;
 use MauticPlugin\SkeletonBundle\Integration\SkeletonBundleIntegration;
 
-class ConfigSupport extends SkeletonBundleIntegration implements ConfigFormInterface
+class ConfigSupport extends SkeletonBundleIntegration implements ConfigFormInterface, ConfigFormFeatureSettingsInterface
 {
     use DefaultConfigFormTrait;
+
+    public function getFeatureSettingsConfigFormName(): string
+    {
+        return ConfigFeaturesType::class;
+    }
 }
